a7c107534d9b49dcd3bd48f36e4b6bfcf5cd6450
alexm <alex.mecklin@hotmail.com>
2025-02-22 01:36:42 +0200

    working for real now

--- dab-overarching-project/client/astro.config.mjs
+++ dab-overarching-project/client/astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte"; //Not working
 import deno from "@deno/astro-adapter";
+import svelte from "@astrojs/svelte";
 
 export default defineConfig({
   adapter: deno(),
-  integrations: [svelte()], //Not working
+  integrations: [svelte()]
 });
